ba58b24f245fee7e2d2db8bd2b379f46b3fd9134
MD RAKIB CHOWDHURY <135139732+rakibCHD@users.noreply.github.com>
2024-05-14 00:55:35 +0600

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -9,7 +9,7 @@ def subtract(x, y):
 # This function multiplies two numbers
 def multiply(x, y):
     return x * y
-   
+    
 # This function divides two numbers
 def divide(x, y):
     return x / y 
