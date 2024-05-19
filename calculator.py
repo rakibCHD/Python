3208a4932c72bccc212c12e5c9a37ca26b4ff57d
MD RAKIB CHOWDHURY <135139732+rakibCHD@users.noreply.github.com>
2024-05-19 16:09:33 +0600

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,7 +1,7 @@
 # This function adds two numbers
 def add(x, y):
     return x + y 
-                
+                 
 # This function subtracts two numbers
 def subtract(x, y):
     return x - y 
