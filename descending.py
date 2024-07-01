fb7153fc304f8b9e62ace242268cd81507fc1826
MD RAKIB CHOWDHURY <135139732+rakibCHD@users.noreply.github.com>
2024-07-02 01:03:20 +0600

    Add files via upload

---
+++ descending.py
@@ -0,0 +1,7 @@
+def sort_descending(numbers):
+    return sorted(numbers, reverse=True)
+
+user_input = input("Enter numbers separated by spaces: ")
+numbers = list(map(int, user_input.split()))
+descending_order = sort_descending(numbers)
+print(f"List in descending order: {descending_order}")
