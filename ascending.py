b18ae7b0ca9f66b6a0299d1f62579b8e7819b3ce
MD RAKIB CHOWDHURY <135139732+rakibCHD@users.noreply.github.com>
2024-07-02 01:02:53 +0600

    Add files via upload

---
+++ ascending.py
@@ -0,0 +1,7 @@
+def sort_ascending(numbers):
+    return sorted(numbers)
+
+user_input = input("Enter numbers separated by spaces: ")
+numbers = list(map(int, user_input.split()))
+ascending_order = sort_ascending(numbers)
+print(f"List in ascending order: {ascending_order}")
