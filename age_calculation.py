478fd13cf93c223b92bd82248f8eb9cedce1a996
MD RAKIB CHOWDHURY <135139732+rakibCHD@users.noreply.github.com>
2024-07-03 00:57:40 +0600

    Add files via upload

---
+++ age_calculation.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+
+def calculate_age(birthdate):
+    today = datetime.today()
+    age = today.year - birthdate.year - ((today.month, today.day) < (birthdate.month, birthdate.day))
+    return age
+
+birthdate_input = input("Enter your birthdate (YYYY-MM-DD): ")
+birthdate = datetime.strptime(birthdate_input, "%Y-%m-%d")
+age = calculate_age(birthdate)
+print(f"Your age is: {age} years")
