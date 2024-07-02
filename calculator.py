34e0892361b2fb07105f82968d46b6173ebc9908
MD RAKIB CHOWDHURY <135139732+rakibCHD@users.noreply.github.com>
2024-07-03 00:53:37 +0600

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,16 +1,12 @@
-# This function adds two numbers
-def add(x, y):
+def add(x, y): 
     return x + y  
          
-# This function subtracts two numbers
 def subtract(x, y):
     return x - y 
            
-# This function multiplies two numbers
 def multiply(x, y):
     return x * y
     
-# This function divides two numbers
 def divide(x, y):
     return x / y 
   
@@ -21,10 +17,8 @@ print("3.Multiply")
 print("4.Divide") 
  
 while True:
-    # take input from the user
     choice = input("Enter choice(1/2/3/4): ")
 
-    # check if choice is one of the four options
     if choice in ('1', '2', '3', '4'):
         try:
             num1 = float(input("Enter first number: "))
@@ -45,8 +39,6 @@ while True:
         elif choice == '4':
             print(num1, "/", num2, "=", divide(num1, num2))
         
-        # check if user wants another calculation
-        # break the while loop if answer is no
         next_calculation = input("Let's do next calculation? (yes/no): ")
         if next_calculation == "no":
           break
