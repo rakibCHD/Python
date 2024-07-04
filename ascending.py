976437b03131ebbf19cac252af57f1ef181cc763
MD RAKIB CHOWDHURY <135139732+rakibCHD@users.noreply.github.com>
2024-07-04 13:31:54 +0600

    Update ascending.py

--- ascending.py
+++ ascending.py
@@ -5,3 +5,4 @@ user_input = input("Enter numbers separated by spaces: ")
 numbers = list(map(int, user_input.split()))
 ascending_order = sort_ascending(numbers)
 print(f"List in ascending order: {ascending_order}")
+ 
