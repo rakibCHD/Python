d2ae8f5dc610c565dd0cdb910d82c764d979fe89
MD RAKIB CHOWDHURY <135139732+rakibCHD@users.noreply.github.com>
2024-07-12 00:40:39 +0600

    Add files via upload

---
+++ gcd.py
@@ -0,0 +1,13 @@
+def main():
+    print("\nInput two numbers to find GCD:")
+    n1 = int(input("    "))
+    n2 = int(input("    "))
+
+    for i in range(1, min(n1, n2) + 1):
+        if n1 % i == 0 and n2 % i == 0:
+            g = i
+
+    print("Greatest Common Divisor (GCD) is:", g)
+
+if __name__ == "__main__":
+    main()
