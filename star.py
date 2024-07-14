fa025bf01990a6645a6feac303e00032e6762ecb
MD RAKIB CHOWDHURY <135139732+rakibCHD@users.noreply.github.com>
2024-07-14 15:33:58 +0600

    Add files via upload

---
+++ star.py
@@ -0,0 +1,10 @@
+def main():
+    r = int(input("Input number of rows to see Star: "))
+
+    for i in range(1, r + 1):
+        for j in range(i):
+            print("*", end="")
+        print()
+
+if __name__ == "__main__":
+    main()
