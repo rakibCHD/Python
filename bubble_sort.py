f57f43a32d7f1269f8962cc156c608a2633ee6f1
MD RAKIB CHOWDHURY <135139732+rakibCHD@users.noreply.github.com>
2024-07-26 12:12:06 +0600

    Update bubble_sort.py

--- bubble_sort.py
+++ bubble_sort.py
@@ -6,7 +6,7 @@ def bubble_sort_ascending(arr):
                 arr[j], arr[j+1] = arr[j+1], arr[j]
     return arr
 
-def bubble_sort_descending(arr):
+def bubble_sort_descending(arr): 
     n = len(arr)
     for i in range(n):
         for j in range(0, n-i-1):
