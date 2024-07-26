f6644355e1d036a8f4cc4984da29b02ec11b82a6
MD RAKIB CHOWDHURY <135139732+rakibCHD@users.noreply.github.com>
2024-07-26 12:11:50 +0600

    Update bubble_sort.py

--- bubble_sort.py
+++ bubble_sort.py
@@ -18,7 +18,7 @@ user_input = input("Enter numbers separated by spaces: ")
 numbers = list(map(int, user_input.split()))
 
 ascending_order = bubble_sort_ascending(numbers[:])
-descending_order = bubble_sort_descending(numbers[:])
+descending_order = bubble_sort_descending(numbers[:]) 
 
 print(f"List in ascending order: {ascending_order}")
 print(f"List in descending order: {descending_order}")
