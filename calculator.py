53a719ad8af8cdd0d3e5699de177c267dc0ce309
MD RAKIB CHOWDHURY <135139732+rakibCHD@users.noreply.github.com>
2024-09-28 23:36:02 +0600

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -14,7 +14,7 @@ print("Select operation.")
 print("1.Add") 
 print("2.Subtract")
 print("3.Multiply")
-print("4.Divide") 
+print("4.Divide")  
   
 while True:
     choice = input("Enter choice(1/2/3/4): ")
