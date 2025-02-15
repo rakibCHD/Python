f892d2bdb34a748617f77813af77c7c36787412b
MD RAKIB CHOWDHURY <135139732+rakibCHD@users.noreply.github.com>
2025-02-15 19:51:16 +0600

    Update descending.py

--- descending.py
+++ descending.py
@@ -5,7 +5,7 @@ user_input = input("Enter numbers separated by spaces: ")
 numbers = list(map(int, user_input.split()))
 descending_order = sort_descending(numbers)
 print(f"List in descending order: {descending_order}")
- 
+  
    
     
   
